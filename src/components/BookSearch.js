f942a0ac365bb59ccde93e7fe06f4a3e3d1811b3
Victoria <victoriahope5683@gmail.com>
2023-03-14 21:28:29 -0500

    books styling and add button

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Dropdown from 'react-bootstrap/Dropdown';
 import './BookSearch.css';
 
 const BookSearch = () => {
@@ -20,7 +21,16 @@ const BookSearch = () => {
         <div id="books-container">
             {books ? books.map((book, index) =>
                 <div key={index} className="book">
-                    <button>+</button>
+                    <Dropdown className="book-dropdown">
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                            Add
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">Unread</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Read</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                     {book.volumeInfo.imageLinks ? <img src={book.volumeInfo.imageLinks.thumbnail} /> : <img src="https://cdn.bookauthority.org/dist/images/book-cover-not-available.6b5a104fa66be4eec4fd16aebd34fe04.png" />}
                     <h1><a>{book.volumeInfo.title}</a></h1>
                     <h2>by {book.volumeInfo.authors[0]}</h2>
