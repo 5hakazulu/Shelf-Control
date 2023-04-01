e3c4c1c4f1b86326e8c77aa68721c70098bcd567
ojichon <misohero@gmail.com>
2023-04-01 10:50:52 -0400

    Savedbooks function is working now

--- src/App.js
+++ src/App.js
@@ -1,52 +1,50 @@
-
-import SControlNavbar from './components/navbar';
-import './App.css';
+import { useContext } from "react";
+import SControlNavbar from "./components/navbar";
+import "./App.css";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import BookSearch from './components/BookSearch';
+import BookSearch from "./components/BookSearch";
 import Footer from "./components/Footer";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import SControlBookClub from './components/BookClub';
+import SControlBookClub from "./components/BookClub";
+import { AuthContext } from "./context/authContext";
 
-
-
-
-
-import Home from './pages/Home';
-
+import Home from "./pages/Home";
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
+
   return (
-
-
     <Router>
       <div className="App">
-        <Switch>
-
-          <Route exact path='/'>
-            <Login />
-            <Footer />
-          </Route>
-          <Route path='/home'>
-            <Home />
-          </Route>
-          <Route path='/sign-up'>
-            <SignUp />
-            <Footer />
-          </Route>
-          <Route path='/bookClub'>
-            <SControlNavbar />
-            <SControlBookClub />
-            <Footer />
-          </Route>
-
-        </Switch>
+        {!currentUser ? (
+          <Switch>
+            <Route exact path="/">
+              <Login />
+              <Footer />
+            </Route>
+            <Route path="/sign-up">
+              <SignUp />
+              <Footer />
+            </Route>
+          </Switch>
+        ) : (
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/bookClub">
+              <SControlNavbar />
+              <SControlBookClub />
+              <Footer />
+            </Route>
+          </Switch>
+        )}
       </div>
     </Router>
-
   );
 }
 
--- src/context/authContext.js
+++ src/context/authContext.js
@@ -5,34 +5,35 @@ export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(
-    null
-    // JSON.parse(localStorage.getItem("user")) || null
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+  const [currentUserId, setUserId] = useState(
+    JSON.parse(localStorage.getItem("userId")) || null
+  );
+  const [currentToken, setCurrentToken] = useState(
+    JSON.parse(localStorage.getItem("token")) || null
   );
 
-
-  
-  
   const login = async (credentials) => {
     try {
-      console.log("Hello")
-      const response = await axios.post('/api/auth/login', credentials);
-  
+      console.log("Hello");
+      const response = await axios.post("/api/auth/login", credentials);
+
       if (response.status === 200) {
-        console.log(response)
         const token = response.data.token;
         const user = credentials.email;
+
         if (token) {
-          localStorage.setItem("token", token);
-          setCurrentUser(user); 
+          localStorage.setItem("user", JSON.stringify(user));
+          localStorage.setItem("token", JSON.stringify(token));
+          setCurrentUser(user);
+          setCurrentToken(token);
         } else {
           console.log("No token received");
         }
       } else {
-       
       }
-    } catch (error) {
-     
-    }
+    } catch (error) {}
   };
 
   const logout = async (inputs) => {
@@ -40,14 +41,45 @@ export const AuthContextProvider = ({ children }) => {
     setCurrentUser(null);
   };
 
-  useEffect(() => {
-    localStorage.setItem("user", JSON.stringify(currentUser));
-  }, [currentUser]);
+  const fetchUserId = async () => {
+    const getToken =
+      currentToken || (JSON.parse(localStorage.getItem("token")) ?? null);
+
+    if (!getToken) {
+      console.log("No token found");
+      return;
+    }
+
+    try {
+      const response = await fetch("/api/user", {
+        headers: {
+          Authorization: `Bearer ${getToken}`,
+        },
+      });
+
+      const data = await response.json();
+
+      if (data?.user?.id) {
+        localStorage.setItem("userId", data.user.id);
+        setUserId(data.user.id);
+      }
+    } catch (error) {
+      console.error("Error fetching user:", error);
+    }
+  };
 
   return (
-    <AuthContext.Provider value={{ currentUser, login, logout }}>
+    <AuthContext.Provider
+      value={{
+        currentUser,
+        currentUserId,
+        currentToken,
+        login,
+        logout,
+        fetchUserId,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
 };
-
